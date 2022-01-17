a1c3ad9260b2f8355892119a4be462572d2e199a
lcy0x1 <arthurlcy0x1@gmail.com>
2022-01-17 15:14:48 -0800

    fix json dump

--- training/ray_training.py
+++ training/ray_training.py
@@ -23,6 +23,9 @@ if __name__ == '__main__':
         if i % 100 == 0:
             checkpoint = trainer.save()
             print("checkpoint saved at", checkpoint)
+            status = {"policy_loss": str(status["policy_loss"]),
+                      "value_function_loss": str(status["vf_loss"]),
+                      "total_loss": str(status["total_loss"])}
             log_list.append({"step": i, "status": status, "checkpoint": checkpoint})
             out_file = open("log.json", "w")
             json.dump(log_list, out_file)
