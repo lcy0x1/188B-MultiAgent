d08239b8b8a461087785e4fb7a5d440c8ee3c8c9
lcy0x1 <arthurlcy0x1@gmail.com>
2022-01-24 14:44:03 -0800

    .

--- training/ray_test_eval.py
+++ training/ray_test_eval.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     trainer = ppo.PPOTrainer(config=tf_config, env="gym_vehicle")
     #log_file = open("./gym_log.json", "r")
     #logs = json.load(log_file)
-    trainer.restore("../../ray_results/PPO_gym_vehicle_2022-01-18_00-25-13gc7rendc/checkpoint_000501/")#logs[-1]["checkpoint"])
+    trainer.restore("../../ray_results/PPO_gym_vehicle_2022-01-18_00-25-13gc7rendc/checkpoint_000501/checkpoint-501")#logs[-1]["checkpoint"])
     env = envs.VehicleEnv(config_data, 0)
     env.reset()
     obs = env.to_observation()
