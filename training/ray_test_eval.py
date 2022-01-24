c8b53bd4145eaebbf944ad4991aa09c482f29878
lcy0x1 <arthurlcy0x1@gmail.com>
2022-01-24 14:50:15 -0800

    .

--- training/ray_test_eval.py
+++ training/ray_test_eval.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     n = 10000
     for i in range(n):
         print("step: ", i)
-        action = trainer.compute_action(obs)
+        action = trainer.compute_single_action(obs)
         obs, reward, done, info = env.step(action)
         total_reward += reward
     print(total_reward / n)
