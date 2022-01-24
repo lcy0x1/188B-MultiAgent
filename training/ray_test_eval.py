b7ff869ae26b6fd5433bdf4d41ca2bf24497946d
lcy0x1 <arthurlcy0x1@gmail.com>
2022-01-24 15:14:56 -0800

    .

--- training/ray_test_eval.py
+++ training/ray_test_eval.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     register_env('gym_vehicle', lambda config: envs.VehicleEnv(config_data, 0))
     tf_config = ppo.DEFAULT_CONFIG.copy()
     tf_config["explore"] = False
+    print(tf_config["normalize_actions"])
     trainer = ppo.PPOTrainer(config=tf_config, env="gym_vehicle")
     trainer.restore("../../ray_results/PPO_gym_vehicle_2022-01-18_00-25-13gc7rendc/checkpoint_000501/checkpoint-501")
     env = envs.VehicleEnv(config_data, 0)
@@ -21,10 +22,8 @@ if __name__ == '__main__':
     n = 10000
     print(env.action_space)
     for i in range(n):
-        print("step: ", i)
-        print(obs)
         action = trainer.compute_single_action(obs)
-        print(action)
+        # TODO clip action?
         obs, reward, done, info = env.step(action)
         total_reward += reward
     print(total_reward / n)
