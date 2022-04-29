3f26cfc69ff6d082e73bb5877350ef255727c0ad
lcy0x1 <arthurlcy0x1@gmail.com>
2022-04-29 11:07:31 -0700

    delete

--- training/imitate.py
+++ training/imitate.py
@@ -11,13 +11,16 @@ imitate = Imitated(env)
 n = 1000
 cumulative_reward = 0
 cumulative_square = 0
+average_price = 0
 for cycle in range(n):
     action = imitate.compute_action()
     _, reward, _, info = env.cycle_step(action)
     reward = reward
     cumulative_reward += reward
     cumulative_square += reward ** 2
+    average_price += info["price"]
 
 mean = cumulative_reward / n
 var = cumulative_square / n - mean ** 2
 print("mean: ", mean, "stdev: ", math.sqrt(var / n))
+print("average price", average_price / n)
