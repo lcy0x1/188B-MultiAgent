a1df8371c063bb9291bee8696309a58faca93670
vfinley <59670689+vfinley@users.noreply.github.com>
2022-05-03 20:42:39 -0700

    print action array 5/3/22

--- training/imitate.py
+++ training/imitate.py
@@ -4,23 +4,22 @@ from gym_symmetric.envs.symmetric_env import VehicleEnv, Imitated
 
 env = VehicleEnv()
 env.reset()
-env.seed(5)
-# random.randint(0, 1000000)
+env.seed(random.randint(0, 1000000))
 imitate = Imitated(env)
 
 n = 1000
 cumulative_reward = 0
 cumulative_square = 0
 average_price = 0
-action = [[] for i in range(env.node)]
 
 for cycle in range(n):
     action = imitate.compute_action()
+    print(action)
     _, reward, _, info = env.cycle_step(action)
     reward = reward
     cumulative_reward += reward
     cumulative_square += reward ** 2
-    average_price += info["price"] * 40
+    average_price += info["price"]
 
 mean = cumulative_reward / n
 var = cumulative_square / n - mean ** 2
